f9a603bdf31cc6c0c5d630f5ab13e239b4ace0fb
IT24101397 <it24101397@my.sliit.lk>
2025-09-19 13:15:10 +0530

    Initial project setup

---
+++ src/main/java/com/webechannelingsystem/web_echannelingsystem/WebEChannelingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.webechannelingsystem.web_echannelingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEChannelingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebEChannelingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webechannelingsystem/web_echannelingsystem/WebEChannelingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webechannelingsystem.web_echannelingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEChannelingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
