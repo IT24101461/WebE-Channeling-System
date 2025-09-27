349739ba06985817b759ae349a87e1b793a9b3d9
Oshadi J.A.A <it24103547@my.sliit.lk>
2025-09-27 11:20:55 +0530

    bug fix in email sending

--- webechannelingsystem/src/main/java/com/webechannelingsystem/webechannelingsystem/notification/EmailService.java
+++ webechannelingsystem/src/main/java/com/webechannelingsystem/webechannelingsystem/notification/EmailService.java
@@ -13,7 +13,7 @@ public class EmailService {
 
     public void sendSimpleEmail(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("sandaruwanperer@gmail.com"); // Set your sender email
+
         message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
